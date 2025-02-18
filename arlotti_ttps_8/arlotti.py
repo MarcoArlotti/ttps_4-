f85adccec8e64a17d0224a4f45d416e3857b9833
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2025-02-18 15:55:35 +0100

    fix

--- arlotti_ttps_8/arlotti.py
+++ arlotti_ttps_8/arlotti.py
@@ -2,12 +2,10 @@ import threading
 
 def prendo_n1(lista_risultati,i):
     n1 = lista_risultati[i]
-    print(n1)
     return n1
 
 def prendo_n2(lista_risultati,i):
     n2 = lista_risultati[i + 1]
-    print(n2)
     return n2
 
 def fai_calcolo(lista_risultati,n1,n2):
@@ -21,19 +19,19 @@ def main():
 
     for j in range(16):
         # Creazione dei thread
-        # thread1 = threading.Thread(target=prendon1, args=(lista_risultati,i))
-        # thread2 = threading.Thread(target=prendon2, args=(lista_risultati,i))
-        n1 = prendo_n1(lista_risultati,i)
-        n2 = prendo_n2(lista_risultati,i)
-        fai_calcolo(lista_risultati,n1,n2)
-        i = i + 1
+
+        thread1 = threading.Thread(target=prendo_n1, args=(lista_risultati,i))
+        thread2 = threading.Thread(target=prendo_n2, args=(lista_risultati,i))
         # Avvio dei thread
-        # thread1.start()
-        # thread2.start()
+        thread1.start()
+        thread2.start()
 
         # Attesa del completamento dei thread
-        # thread1.join()
-        # thread2.join()
+        thread1.join()
+        thread2.join()
+
+        fai_calcolo(lista_risultati,n1,n2)
+        i = i + 1
 
         # Stampa dei risultati
         print(lista_risultati)
